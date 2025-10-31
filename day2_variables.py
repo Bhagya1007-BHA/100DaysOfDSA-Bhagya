4f48c80d8343cc22262b09bea8cb900fe1a53a9b
bhagyalaxmi <sabavathbhagyalaxmi@gmail.com>
2025-10-31 20:55:15 +0530

    Add variables and data types examples in Python

---
+++ day2_variables.py
@@ -0,0 +1,29 @@
+# Day 2: Variables and Data Types in Python
+
+# Integer
+age = 19
+
+# Float
+height = 5.4
+
+# String
+name = "Bhagya"
+
+# Boolean
+is_student = True
+
+# Type Casting
+age_str = str(age)
+
+# Print all values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+print("Age (as string):", age_str)
+
+# Checking data types
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
